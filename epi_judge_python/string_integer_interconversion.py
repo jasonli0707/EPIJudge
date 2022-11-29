5574edee251b512946710295ea555902235530e2
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2022-11-29 22:29:13 +0800

    question 6.1

--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -1,16 +1,39 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
-
+import functools
+import string
 
 def int_to_string(x: int) -> str:
-    # TODO - you fill in here.
-    return '0'
+    """
+    Time: O(n)
+    Space: O(n)
+    """
+    s = [] # mutable list
+    is_negative = False
+    if x < 0:
+        x, is_negative = -x, True
+
+    while True:
+        # ord() get integer representation from ascii table
+        # x%10 extracts least significant digit
+        s.append(chr(ord('0') + x%10)) 
+        x//=10
+        if x == 0:
+            break
+
+    if is_negative:
+       s.append('-') 
+
+    return ''.join(reversed(s)) # immutable string
 
 
 def string_to_int(s: str) -> int:
-    # TODO - you fill in here.
-    return 0
-
+    """
+    Time: O(n)
+    Space: O(n)
+    """
+    # The function in functools.reduce() takes two arguments (1) cumulative result (2) next element from list and applies iteratively to the list
+    return functools.reduce(lambda running_sum, c: 10*running_sum + string.digits.index(c), s[(s[0]== '-' or s[0] == '+'):], 0) * (-1 if s[0]=='-' else 1)
 
 def wrapper(x, s):
     if int(int_to_string(x)) != x:

3ff572f8da38781d88eb08ff49b94c629371b332
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2022-11-29 22:26:59 +0800

    question 5.18

--- epi_judge_python/spiral_ordering.py
+++ epi_judge_python/spiral_ordering.py
@@ -4,8 +4,26 @@ from test_framework import generic_test
 
 
 def matrix_in_spiral_order(square_matrix: List[List[int]]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    """
+    Time Complexity: O(n^2)
+    """
+    directions = [(0,1), (1,0), (0,-1), (-1,0)]
+    d = x = y = 0
+    spiral_ordering = []
+
+    for _ in range(len(square_matrix)**2):
+        spiral_ordering.append(square_matrix[x][y])
+        square_matrix[x][y] = 0
+
+        next_x, next_y = x + directions[d][0], y + directions[d][1]
+        if (next_x not in range(len(square_matrix)) or next_y not in range(len(square_matrix)) or square_matrix[next_x][next_y]==0):
+            # skip out of range indices or elements which have been visited
+            d = (d+1) & 3 # select first two bits i.e. 5 & 3 = 101 & 011 -> 01 = 1
+            next_x, next_y = x + directions[d][0], y+directions[d][1]
+
+        x, y = next_x, next_y
+
+    return spiral_ordering
 
 
 if __name__ == '__main__':

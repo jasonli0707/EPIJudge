468f8e67408fc443978a47577fd714efb0ddc5c1
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2022-12-01 00:14:26 +0800

    question 6.5

--- epi_judge_python/is_string_palindromic.py
+++ epi_judge_python/is_string_palindromic.py
@@ -2,7 +2,23 @@ from test_framework import generic_test
 
 
 def is_palindromic(s: str) -> bool:
-    # TODO - you fill in here.
+    '''
+    Time: O(n)
+    Space: O(1)
+    '''
+    i = 0
+    j = len(s) - 1
+    while i<j:
+        if s[i] == ' ':
+            i += 1
+            continue
+        if s[j] == ' ':
+            j -= 1
+            continue
+        if s[i].lower() != s[j].lower():
+            return False
+        i += 1
+        j -= 1
     return True
 
 

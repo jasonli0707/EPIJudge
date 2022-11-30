feae05775a91ed7575f1c148e9f3c38edcb9ab1d
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2022-12-01 01:02:13 +0800

    question 6.6

--- epi_judge_python/reverse_words.py
+++ epi_judge_python/reverse_words.py
@@ -6,9 +6,39 @@ from test_framework.test_utils import enable_executor_hook
 
 # Assume s is a list of strings, each of which is of length 1, e.g.,
 # ['r', 'a', 'm', ' ', 'i', 's', ' ', 'c', 'o', 's', 't', 'l', 'y'].
+
+
 def reverse_words(s):
-    # TODO - you fill in here.
-    return
+    '''
+    Time: O(1)
+    Space: O(n)
+    '''
+    # reverse the whole string
+    s.reverse()
+
+    def reverse_a_word(s, start, end):
+        while start < end:
+            s[start], s[end] = s[end], s[start]
+            start += 1
+            end -= 1
+
+    # reverse each word
+    start = 0
+    while True:
+        end = -1
+        for i in range(start, len(s)):
+            if s[i] == ' ':
+                end = i
+                break
+        if end < 0: # end=-1 when no white space is found
+            break
+        reverse_a_word(s, start, end - 1)  
+        start = end + 1
+
+    # reverse the last word
+    reverse_a_word(s, start, len(s)-1)
+
+
 
 
 @enable_executor_hook
@@ -21,6 +51,8 @@ def reverse_words_wrapper(executor, s):
 
 
 if __name__ == '__main__':
+    s= [' ', ' ', '9', 'Z', 'o', 'A', 'z', 'q']
+    reverse_words(s)
     exit(
         generic_test.generic_test_main('reverse_words.py', 'reverse_words.tsv',
                                        reverse_words_wrapper))

793424d31d2e0938aafdd679e48af6d9eb590036
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2022-12-02 14:25:05 +0800

    question 6.8

--- epi_judge_python/look_and_say.py
+++ epi_judge_python/look_and_say.py
@@ -1,9 +1,35 @@
 from test_framework import generic_test
+import itertools
 
+
+
+def look_and_say_pythonic(n):
+    # groupby() groups consecutive sequence
+    s = '1'
+    for _ in range(n-1):
+        s = ''.join([str(len(list(group))) + key for key, group in itertools.groupby(s)])
+    return s
 
 def look_and_say(n: int) -> str:
-    # TODO - you fill in here.
-    return ''
+    '''
+    Time: O(n*2^n) upper bound
+    Explanation: 
+    next number is at most twice as many digits as prev one when all digits are distinct => max length is smaller or equal to 2^n
+    each iteration is proportion to the number of digits n in the string s 
+    '''
+    s = '1'
+    for _ in range(n-1):
+        i = 0
+        result = []
+        while i < len(s): # one particular group
+            count = 1
+            while i + 1 < len(s) and s[i] == s[i+1]: # consecutive equal digit
+                count += 1
+                i += 1
+            result.append(str(count) + s[i])
+            i += 1
+        s = ''.join(result)
+    return s
 
 
 if __name__ == '__main__':

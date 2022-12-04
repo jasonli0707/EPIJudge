948b3c64bd319fda3a48131bec18a6108a5cb69a
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2022-12-04 15:11:29 +0800

    question 6.9

--- epi_judge_python/roman_to_integer.py
+++ epi_judge_python/roman_to_integer.py
@@ -1,9 +1,25 @@
 from test_framework import generic_test
 
 
+MAPPING = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
 def roman_to_integer(s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    '''
+    Time: O(n)
+    Space: O(1)
+    '''
+    sum = i = 0
+    prev = 1001
+    while i < len(s):
+        curr = MAPPING[s[i]] 
+        if  curr > prev:
+            sum = sum + curr - 2*prev
+        else:
+            sum += curr
+        prev = MAPPING[s[i]]
+        i += 1
+
+    return sum
 
 
 if __name__ == '__main__':

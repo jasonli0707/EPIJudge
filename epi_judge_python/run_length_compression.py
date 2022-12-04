3ef435c4228b6c787e77921e353611bc93668433
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2022-12-04 16:11:13 +0800

    question 6.12

--- epi_judge_python/run_length_compression.py
+++ epi_judge_python/run_length_compression.py
@@ -1,15 +1,35 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
+import itertools
+import string
 
 
 def decoding(s: str) -> str:
-    # TODO - you fill in here.
-    return ''
-
+    result = []
+    count = 0
+    for c in s:
+        if c.isdigit():
+            count = count*10 + int(c)
+        else:
+            result.append(c*count)
+            count = 0
+    return ''.join(result)
 
 def encoding(s: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    i, count = 0, 1
+    result = []
+    while i+1 < len(s):
+        if s[i] == s[i+1]:
+            count += 1
+        else:
+            result.append(str(count) + s[i])
+            count = 1
+        i+=1
+    result.append(str(count) + s[i])
+    return ''.join(result)
+
+def encoding_pythonic(s: str) -> str:
+    return ''.join([str(len(list(g))) + k for k, g in itertools.groupby(s)])
 
 
 def rle_tester(encoded, decoded):

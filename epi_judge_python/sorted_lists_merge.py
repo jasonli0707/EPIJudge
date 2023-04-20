9a5e1195446306ff5582c16f6b0a065b92fc4d7c
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2023-04-21 00:32:13 +0800

    question 7.1

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -6,8 +6,26 @@ from test_framework import generic_test
 
 def merge_two_sorted_lists(L1: Optional[ListNode],
                            L2: Optional[ListNode]) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    """merge two sorted linked lists
+       Time: worst-case: O(n+m)
+       Space: O(1)
+
+    Returns:
+        Optional[ListNode]: the head of the merged linked list
+    """
+    dummy_head = tail = ListNode() # referencing to the same address
+
+    while L1 and L2: # break when either one or both are empty
+        if L1.data < L2.data:
+            tail.next, L1 = L1, L1.next
+        else:
+            tail.next, L2 = L2, L2.next
+
+        tail = tail.next # move tail pointer to the next node in the merged list
+
+    tail.next = L1 or L2 # appends the remaining nodes of L1 or L2
+    
+    return dummy_head.next
 
 
 if __name__ == '__main__':

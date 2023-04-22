fa02845a6b0ceaad02e3066830b9255f41d0dcfe
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2023-04-22 23:03:36 +0800

    question 7.7

--- epi_judge_python/delete_kth_last_from_list.py
+++ epi_judge_python/delete_kth_last_from_list.py
@@ -6,8 +6,27 @@ from test_framework import generic_test
 
 # Assumes L has at least k nodes, deletes the k-th last node in L.
 def remove_kth_last(L: ListNode, k: int) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    """delete the kth last node in L
+
+    Args:
+        L (ListNode): a singly linked list
+        k (int): index of kth last node 
+
+    Returns:
+        Optional[ListNode]: linked list with k-th node deleted 
+    """
+    first = L
+    second = dummy_head = ListNode(0, L) # (k+1)-th node to delete k-th node
+
+    for _ in range(k):
+        first = first.next
+
+    while first:
+        first, second = first.next, second.next
+
+    second.next = second.next.next
+
+    return dummy_head.next # dummy head is necessary as the first node can be the k-th last node
 
 
 if __name__ == '__main__':

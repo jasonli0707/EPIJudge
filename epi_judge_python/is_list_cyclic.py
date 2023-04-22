1adaec4266f787b66cb878fd66243432eea40c3b
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2023-04-22 21:07:31 +0800

    question 7.3

--- epi_judge_python/is_list_cyclic.py
+++ epi_judge_python/is_list_cyclic.py
@@ -7,8 +7,60 @@ from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
+def hash_solution(head):
+    # O(n) time and O(n) space solution with hash table
+    visited = set()
+
+    current = head
+    while current:
+        if id(current) in visited:
+            return current
+        else:
+            visited.add(id(current))
+        current = current.next
+
+    return None
+
+
+def brute_force(head):
+    # O(n^2) time and O(1) space solution
+    outer = head
+    i = 0
+    while outer:
+        outer = outer.next
+        i += 1
+        inner = head
+        for _ in range(i):
+            if id(inner) == id(outer):
+                return outer
+            inner = inner.next
+    return None
+
 def has_cycle(head: ListNode) -> Optional[ListNode]:
-    # TODO - you fill in here.
+    """Floyd's Cycle-Finding algorithm  or Hare-Tortoise algorithm
+        It is also called Hare-Tortoise algorithm
+        The algorithm works by using two pointers, a slow pointer and a fast pointer.
+        Initially, both pointers are set to the head of the linked list.
+        The fast pointer moves twice as fast as the slow pointer.
+        If there is a cycle in the linked list, eventually, the fast pointer will catch up with the slow pointer.
+        If there is no cycle, the fast pointer will reach the end of the linked list.
+
+       O(n) Time and O(1) Space
+    Args:
+        head (ListNode): A singly linked list
+
+    Returns:
+        Optional[ListNode]: return None if no cycle is found else return the node where the cycle begins.
+    """
+    fast = slow = head
+    while fast and fast.next and fast.next.next: # need at least two nodes to form a cycle
+        slow, fast = slow.next, fast.next.next # fast advances twice as fast
+        if slow is fast: # cycle exits when two meets
+            slow = head # reset
+            while slow is not fast:
+                slow, fast = slow.next, fast.next
+            return slow
+
     return None
 
 

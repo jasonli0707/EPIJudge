7c2d1ad458e3564596c244ceac22c555cff50375
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2023-04-25 00:30:59 +0800

    question 8.1

--- epi_judge_python/stack_with_max.py
+++ epi_judge_python/stack_with_max.py
@@ -1,32 +1,91 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
+from collections import namedtuple
 
 
-class Stack:
+class StackCachedMax:
+    """Stack implementation with max operation using caching
+
+       O(1) time for all operations
+       O(n) additional space for caching max elements
+    """
+    ElementWithCachedMax = namedtuple('ElementWithCachedMax', ['element', 'max']) # similar to struct in C++
+
+    def __init__(self):
+        self._elements_with_cached_max = [] # array of namedtuple
+
     def empty(self) -> bool:
-        # TODO - you fill in here.
-        return True
+        return len(self._elements_with_cached_max) == 0
 
     def max(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        if self.empty():
+            raise IndexError("max(): empty stack")
+        return self._elements_with_cached_max[-1].max
 
     def pop(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        if self.empty():
+            raise IndexError("pop(): empty stack")
+        return self._elements_with_cached_max.pop().element
 
     def push(self, x: int) -> None:
-        # TODO - you fill in here.
-        return
+        self._elements_with_cached_max.append(self.ElementWithCachedMax(x, x if self.empty() else max(x, self.max())))
+
+        
+class StackMaxCount:
+    """Stack implementation with max operation by keep track of max counts
+
+       O(1) time for all operations
+       O(1) best-case and O(n) worst-case additional space for keeping track of max elements
+    """
+    class MaxWithCount: # keep track of the counts of max elements
+        def __init__(self, max, count):
+            self.max = max
+            self.count = count
+
+    def __init__(self):
+        self._s = []
+        self._max_count_s = []
+
+    def empty(self) -> bool:
+        return len(self._s) == 0
+
+    def max(self) -> int:
+        if self.empty():
+            raise IndexError("max(): empty stack")
+        return self._max_count_s[-1].max
+
+    def pop(self) -> int:
+        if self.empty():
+            raise IndexError("pop(): empty stack")
+
+        x = self._s.pop()  
+        current_max = self._max_count_s[-1].max # cannot call self.max() as self._s may already be empty after pop
+        if x == current_max:
+            self._max_count_s[-1].count -= 1
+            if self._max_count_s[-1].count == 0:
+                self._max_count_s.pop()
+        return x
+
+    def push(self, x: int) -> None:
+        self._s.append(x)
+        if len(self._max_count_s) == 0: # cannot call self.empty() as we already push x into self._s 
+            self._max_count_s.append(self.MaxWithCount(x, 1))
+        else:
+            current_max = self.max()
+            if x == current_max:
+                self._max_count_s[-1].count += 1
+            elif x>current_max:
+                self._max_count_s.append(self.MaxWithCount(x, 1))
+
 
 
 def stack_tester(ops):
     try:
-        s = Stack()
+        s = StackMaxCount()
 
         for (op, arg) in ops:
             if op == 'Stack':
-                s = Stack()
+                s = StackMaxCount()
             elif op == 'push':
                 s.push(arg)
             elif op == 'pop':

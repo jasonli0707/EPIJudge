ba8a495417d4ba2113c55001aed24c8f92923500
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2023-04-26 23:44:49 +0800

    question 8.4

--- epi_judge_python/directory_path_normalization.py
+++ epi_judge_python/directory_path_normalization.py
@@ -1,9 +1,30 @@
 from test_framework import generic_test
+import string
 
 
 def shortest_equivalent_path(path: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    if not path:
+        raise ValueError("Invalid Path")
+
+    path_names = []
+
+    # special case when path starts with /
+    if path[0] == '/':
+        path_names.append('/')
+
+
+    for token in (token for token in path.split('/') if token not in ['.', '']): # ignore . or empty token
+        if token == '..':
+            if not path_names or path_names[-1] == '..': # if last path name is .. or empty stack
+                path_names.append(token)
+            else:
+                if path_names[-1] == '/':
+                    raise ValueError("Path Error")
+                path_names.pop()
+        else:
+            path_names.append(token)
+    result = '/'.join(path_names)
+    return result[result.startswith('//'):] # if result start with // => start from index 1 i.e. skip one /
 
 
 if __name__ == '__main__':

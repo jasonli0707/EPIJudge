8bb5e2ed573275cada3dd214026b3c374b4bfc84
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2023-04-28 00:04:47 +0800

    question 8.5

--- epi_judge_python/sunset_view.py
+++ epi_judge_python/sunset_view.py
@@ -1,11 +1,31 @@
 from typing import Iterator, List
+from collections import namedtuple
 
 from test_framework import generic_test
 
 
 def examine_buildings_with_sunset(sequence: Iterator[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    """Process buildings from east-to-west order and returns the set of buildings which view the sunset
+       Time: O(n) 
+       Space: O(1) best-case (increasing height from east-to-west) and O(n) worst-case (decreasing height from east-to-west)
+
+    Args:
+        sequence (Iterator[int]): buildings from east-to-west
+
+    Returns:
+        List[int]: buildings which view the sunset from west-to-east
+    """
+    Building = namedtuple("Building", ['id', 'height'])
+
+    buildings_have_view = []
+
+    for id, height in enumerate(sequence):
+        while buildings_have_view and height >= buildings_have_view[-1].height:
+            buildings_have_view.pop() # remove all buildings of which sunset view are blocked 
+
+        buildings_have_view.append(Building(id, height))
+
+    return [b.id for b in reversed(buildings_have_view)]
 
 
 def examine_buildings_with_sunset_wrapper(sequence):

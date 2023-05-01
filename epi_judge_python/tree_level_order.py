4d41c3e895fae84141d305f001fc302959ed9c63
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2023-05-02 01:04:44 +0800

    question 8.6

--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -5,8 +5,27 @@ from test_framework import generic_test
 
 
 def binary_tree_depth_order(tree: BinaryTreeNode) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    """return the level order traversal of a binary tree nodes' values
+       O(n) Time
+       O(m) Space (i.e. max number of nodes at any single depth stored in curr_depth_nodes)
+    Args:
+        tree (BinaryTreeNode)
+
+    Returns:
+        List[List[int]]: list of list of nodes at each level
+    """
+    results = []
+    if not tree:
+        return results
+
+    curr_depth_nodes = [tree]
+
+    while curr_depth_nodes:
+        results.append([node.data for node in curr_depth_nodes])
+        curr_depth_nodes = [child for node in curr_depth_nodes for child in (node.left, node.right) if child]
+
+    return results
+        
 
 
 if __name__ == '__main__':

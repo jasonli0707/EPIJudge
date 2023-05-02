457eebba2e4d9a4ca9f658b71df8c972cd743c34
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2023-05-03 01:22:18 +0800

    question 8.8

--- epi_judge_python/queue_from_stacks.py
+++ epi_judge_python/queue_from_stacks.py
@@ -3,13 +3,26 @@ from test_framework.test_failure import TestFailure
 
 
 class Queue:
+    """implement a queue from stacks
+       O(1) enqueue 
+       O(1) dequeue if stack 2 is not empty else O(m) dequeue (number of elements in stack 1)
+    """
+    def __init__(self) -> None:
+        self.s1 = []
+        self.s2 = []
+
     def enqueue(self, x: int) -> None:
-        # TODO - you fill in here.
+        self.s1.append(x)
         return
 
     def dequeue(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        if not self.s2: # if empty
+            while self.s1:
+                self.s2.append(self.s1.pop())
+
+        if not self.s2: # still empty
+            raise IndexError('empty queue')
+        return self.s2.pop()
 
 
 def queue_tester(ops):

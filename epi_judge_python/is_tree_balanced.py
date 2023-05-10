0791e9129112065f4f29cce377c7ba6e8b60d810
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2023-05-10 23:57:35 +0800

    question 9.1

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -2,9 +2,43 @@ from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
+def brute_force(root):
+    """brute force solution
+    O(n^2) Time Complexity
+    O(n) Space for call stack
+    """
+    def maxDepth(root):
+        if not root:
+            return 0
+
+        return max(maxDepth(root.left), maxDepth(root.right)) + 1
+
+    if not root:
+        return True
+
+    if abs(maxDepth(root.left) - maxDepth(root.right)) > 1:
+        return False
+
+    return brute_force(root.left) and brute_force(root.right)
+
 def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    """check if the binary tree is height balanced
+       Time: O(n)
+       Space: O(h) # height of tree
+    """
+
+    def dfs(root):
+        if not root: return (True, 0)
+
+        is_balanced_left, height_left = dfs(root.left)
+        is_balanced_right, height_right = dfs(root.right)
+
+        is_balanced = is_balanced_left and is_balanced_right and abs(height_left - height_right) <= 1
+        height = max(height_left, height_right) + 1
+
+        return [is_balanced, height]
+
+    return dfs(tree)[0]
 
 
 if __name__ == '__main__':

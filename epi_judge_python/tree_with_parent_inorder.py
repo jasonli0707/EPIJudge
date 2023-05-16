1a5a6e17829266f73b97eceaf63186be015e38e1
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2023-05-16 22:08:48 +0800

    question 9.11

--- epi_judge_python/tree_with_parent_inorder.py
+++ epi_judge_python/tree_with_parent_inorder.py
@@ -5,8 +5,27 @@ from test_framework import generic_test
 
 
 def inorder_traversal(tree: BinaryTreeNode) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    """Nonrecursive inorder traversal with O(1) Space. Nodes have parent fields.
+       Time: O(n)
+    """
+    prev, result = None, []
+
+    while tree:
+        if prev is tree.parent: # come down to right or left child
+            if tree.left: # left child
+                next = tree.left
+            else: # leaves
+                result.append(tree.data)
+                next = tree.right or tree.parent # if right child is not None, assigns tree.right to next (short circuit)
+        elif tree.left is prev: # come up from left child
+            result.append(tree.data)
+            next = tree.right or tree.parent 
+        else: # come up from right child
+            next = tree.parent
+            
+        prev, tree = tree, next
+
+    return result 
 
 
 if __name__ == '__main__':

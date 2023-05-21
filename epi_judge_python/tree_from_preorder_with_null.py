b058ace2bb454984907d7e1de200a60ccef63dc8
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2023-05-21 21:44:11 +0800

    question 9.13

--- epi_judge_python/tree_from_preorder_with_null.py
+++ epi_judge_python/tree_from_preorder_with_null.py
@@ -6,9 +6,66 @@ from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
 
 
+# Time: O(n)
+
+# 1) using nonlocal to keep track of global idx
 def reconstruct_preorder(preorder: List[int]) -> BinaryTreeNode:
-    # TODO - you fill in here.
-    return BinaryTreeNode()
+    idx = 0
+    def recursion_helper():
+        nonlocal idx  # Declare idx as nonlocal
+        node = preorder[idx]
+        idx += 1
+
+        if node is None:
+            return None
+
+        left_subtree = recursion_helper()
+        right_subtree = recursion_helper()
+       
+        return BinaryTreeNode(data=node,
+                                left=left_subtree,
+                                right=right_subtree
+                                ) 
+
+    return recursion_helper()
+
+# 2) use two local variables next_idx and final_idx
+def reconstruct_preorder2(preorder: List[int]) -> BinaryTreeNode:
+    def recursion_helper(idx):
+        node = preorder[idx]
+        idx += 1
+
+        if node is None:
+            return None, idx
+
+        left_subtree, next_idx = recursion_helper(idx)
+        right_subtree, final_idx = recursion_helper(next_idx)
+
+        return BinaryTreeNode(data=node,
+                              left=left_subtree,
+                              right=right_subtree
+                             ), final_idx
+
+    tree, _ = recursion_helper(0)
+    return tree
+
+# 3) use an iterator with builtin attribute
+def reconstruct_preorder3(preorder: List[int]) -> BinaryTreeNode:
+    def recursion_helper(iter):
+        node = next(iter)
+
+        if node is None:
+            return None
+
+        left_subtree = recursion_helper(iter)
+        right_subtree = recursion_helper(iter)
+
+        return BinaryTreeNode(data=node,
+                              left=left_subtree,
+                              right=right_subtree
+                             )
+
+    return recursion_helper(iter(preorder))
 
 
 @enable_executor_hook

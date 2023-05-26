cd4b3495a21fdf72be97fce84276df87d8f45fbe
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2023-05-27 00:48:10 +0800

    question 10.3

--- epi_judge_python/sort_almost_sorted_array.py
+++ epi_judge_python/sort_almost_sorted_array.py
@@ -1,12 +1,34 @@
 from typing import Iterator, List
+from itertools import islice
+import heapq
 
 from test_framework import generic_test
 
 
-def sort_approximately_sorted_array(sequence: Iterator[int],
-                                    k: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+def sort_approximately_sorted_array(sequence: Iterator[int], k: int) -> List[int]:
+    """_summary_
+
+    Args:
+        sequence (Iterator[int]): a k-sorted list
+        k (int): an element is at most k away from its final sorted position
+
+    Returns:
+        List[int]: sorted list
+    """
+    min_heap = []  # keep track of k + 1 elements
+    result = []
+
+    for item in islice(sequence, k):
+        heapq.heappush(min_heap, item)
+
+
+    for item in sequence: # will start from the (k + 1)th element
+        result.append(heapq.heappushpop(min_heap, item))
+
+    while min_heap:
+       result.append(heapq.heappop(min_heap)) 
+
+    return result
 
 
 def sort_approximately_sorted_array_wrapper(sequence, k):

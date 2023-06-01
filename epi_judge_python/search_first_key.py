89aba78abe5800ec21f129656e00461c17e3b7c3
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2023-06-01 18:46:56 +0800

    question 11.1

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -1,11 +1,28 @@
 from typing import List
+from bisect import bisect_left
 
 from test_framework import generic_test
 
 
 def search_first_of_k(A: List[int], k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    left, right, result = 0, len(A)-1, -1
+    while  left<=right:
+        mid = left + (right-left)//2
+        if A[mid] < k:
+            left = mid + 1
+        elif A[mid] == k:
+            result = mid
+            right = mid - 1
+        else:
+            right = mid - 1
+            
+    return result
+
+
+def search_first_of_k_pythonic(A, k):
+    i = bisect_left(A, k)
+    return i if 0 <= i < len(A) and A[i] == k else -1
+    
 
 
 if __name__ == '__main__':

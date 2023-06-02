df13b6aa705e766969c27a334ec07cb865839071
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2023-06-02 23:47:14 +0800

    question 11.5

--- epi_judge_python/real_square_root.py
+++ epi_judge_python/real_square_root.py
@@ -1,9 +1,21 @@
 from test_framework import generic_test
+import math
 
 
 def square_root(x: float) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    l, u = (1.0, x) if x > 1.0 else (x, 1.0)
+
+    while not math.isclose(l, u):
+        m = l + (u-l)/2
+        m2 = m*m 
+        diff = m2 - x
+        
+        if diff > 0: # m2 > x
+            u = m
+        else: # m2 <= x
+            l = m
+
+    return l
 
 
 if __name__ == '__main__':

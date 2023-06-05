56c1e8d7a7259fec3b7903f25a7841e3ff7c134a
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2023-06-05 15:50:01 +0800

    question 11.6

--- epi_judge_python/search_row_col_sorted_matrix.py
+++ epi_judge_python/search_row_col_sorted_matrix.py
@@ -4,8 +4,22 @@ from test_framework import generic_test
 
 
 def matrix_search(A: List[List[int]], x: int) -> bool:
-    # TODO - you fill in here.
-    return True
+    """
+    Time: O(n+m) i.e. at most n+m-1 steps (eliminates one row or column each step) when x is not present in A
+    """
+   
+    n, m = len(A), len(A[0]) # nxm matrix A
+    r, c = 0, m - 1
+
+    while c>=0 and r<n:
+        if A[r][c] == x:
+            return True
+        elif A[r][c] > x: 
+            c -= 1
+        else:
+            r += 1
+
+    return False
 
 
 if __name__ == '__main__':

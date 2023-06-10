8c2f5b3cc0f80ef0d4f6164c8142e2d4f8297a09
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2023-06-10 16:00:11 +0800

    question 11.9

--- epi_judge_python/absent_value_array.py
+++ epi_judge_python/absent_value_array.py
@@ -2,12 +2,36 @@ from typing import Iterator
 
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
+import itertools
 
 
 def find_missing_element(stream: Iterator[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    NUM_BUCKET = 1 << 16 # 2^16
+    counter = [0] * NUM_BUCKET # 2^16 uint16 = 128 KB
+    stream, stream_copy = itertools.tee(stream)
+    for x in stream:
+        upper_part_x = x >> 16 # extract 16 MSBs
+        counter[upper_part_x] += 1
+   
+    # look for a bucket that contains less than 1 << 16 i.e 2^16 elements
+    BUCKET_CAPACITY = 1 << 16
+    candidate_bucket = next(i for i, c in enumerate(counter) if c < BUCKET_CAPACITY) # a single missing candidate
 
+    # Find all IP addresses in the stream whose first 16 bits are equal to candidate_bucket
+    candidates = [0] * BUCKET_CAPACITY
+    stream = stream_copy
+    for x in stream_copy:
+        upper_part_x = x >> 16
+        if upper_part_x == candidate_bucket: # if MSBs match, then record LSBs
+            lower_part_x = ((1<<16) - 1) & x
+            candidates[lower_part_x] += 1
+
+    # iterate all possible LSBs to find the missing one
+    for i, v in enumerate(candidates):
+        if v == 0: # absent LSBs
+            return (candidate_bucket << 16) | i
+            
+    return -1
 
 def find_missing_element_wrapper(stream):
     try:

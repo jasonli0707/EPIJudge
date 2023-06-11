61e8c99f3b11b51812b2a8da8b2f4bd46885fef8
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2023-06-11 15:33:07 +0800

    question 12.0

--- epi_judge_python/anagrams.py
+++ epi_judge_python/anagrams.py
@@ -1,11 +1,26 @@
 from typing import List
 
 from test_framework import generic_test, test_utils
+from collections import defaultdict, Counter
 
 
 def find_anagrams(dictionary: List[str]) -> List[List[str]]:
-    # TODO - you fill in here.
-    return []
+    """
+    Time: O(nm)
+    Space: O(n)
+    """
+
+    anagram_groups = defaultdict(list)
+
+    for s in dictionary:
+        freq_arr = [0]*26 # 26 characters
+        for c in s: # O(m)
+            if c != " ":
+                freq_arr[ord(c)-97] += 1 
+        key = tuple(freq_arr) # O(1) cast list to hashable tuple
+        anagram_groups[key].append(s)
+
+    return [group for group in anagram_groups.values() if len(group) > 1]
 
 
 if __name__ == '__main__':

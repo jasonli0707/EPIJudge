4e59a1db5691ad4c1f8608b2588b2a459d4faca7
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2023-06-11 16:24:59 +0800

    question 12.2

--- epi_judge_python/is_anonymous_letter_constructible.py
+++ epi_judge_python/is_anonymous_letter_constructible.py
@@ -1,10 +1,28 @@
 from test_framework import generic_test
+from collections import Counter
 
 
 def is_letter_constructible_from_magazine(letter_text: str,
                                           magazine_text: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    """
+    Time: O(n+m)
+    Space: O(L) i.e. L is the number of distinct characters in the letter
+    """
+    letter_counts = Counter(letter_text)
+
+    for c in magazine_text:
+        if c in letter_counts:
+            letter_counts[c] -= 1
+            if letter_counts[c] == 0:
+                del letter_counts[c]
+                if not letter_counts: # already empty i.e. covered
+                    return True
+
+    return not letter_counts  # every character in letter_text is covered by the magazine
+
+def pythonic_sol(letter_text, magazine_text):
+    # only keep postive counts. If counts == 0, then key will be removed
+    return not (Counter(letter_text) - Counter(magazine_text))
 
 
 if __name__ == '__main__':

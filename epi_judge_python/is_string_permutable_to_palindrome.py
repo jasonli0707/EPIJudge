20e24a4b8e0b76113589d434c520b47b5285e4ae
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2023-06-11 16:02:49 +0800

    question 12.1

--- epi_judge_python/is_string_permutable_to_palindrome.py
+++ epi_judge_python/is_string_permutable_to_palindrome.py
@@ -1,9 +1,13 @@
 from test_framework import generic_test
+from collections import Counter
 
 
 def can_form_palindrome(s: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    """
+    Time: O(n)
+    Space: O(c) i.e. c is the number of distinct characters
+    """
+    return sum(v%2 for v in Counter(s).values()) <= 1 # if v is odd, then add 1 to the final result
 
 
 if __name__ == '__main__':

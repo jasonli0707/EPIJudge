4105e8a9796c8f2f830afca1d522ebe0f2c6d444
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2023-06-26 22:34:26 +0800

    question 12.9

--- epi_judge_python/longest_contained_interval.py
+++ epi_judge_python/longest_contained_interval.py
@@ -4,8 +4,29 @@ from test_framework import generic_test
 
 
 def longest_contained_range(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    """
+    Time: O(n)
+    """
+
+    hash_table = set(A)
+    max_range = 0
+    
+    while hash_table:
+        a = hash_table.pop()
+
+        lower = a - 1
+        while lower in hash_table:
+            hash_table.remove(lower)
+            lower -= 1
+
+        upper = a + 1
+        while upper in hash_table:
+            hash_table.remove(upper)
+            upper += 1
+
+        max_range = max(max_range, upper-lower-1)
+
+    return max_range
 
 
 if __name__ == '__main__':

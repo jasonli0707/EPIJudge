9cbe789e73d65017652fb365c4aa4e29a6800235
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2023-07-01 20:04:05 +0800

    question 13.2

--- epi_judge_python/two_sorted_arrays_merge.py
+++ epi_judge_python/two_sorted_arrays_merge.py
@@ -5,8 +5,23 @@ from test_framework import generic_test
 
 def merge_two_sorted_arrays(A: List[int], m: int, B: List[int],
                             n: int) -> None:
-    # TODO - you fill in here.
-    return
+    """
+    Time: O(n+m)
+    """
+
+    i, j, k = m-1, n-1, m+n-1
+    while i >= 0 and j>=0: 
+        if A[i] > B[j]:
+            A[k] = A[i]
+            i -= 1
+        else:
+            A[k] = B[j]
+            j -= 1
+        k -= 1
+  
+    while j >= 0: # only second array is required as first array is already sorted
+        A[k] = B[j]
+        j, k = j - 1, k - 1
 
 
 def merge_two_sorted_arrays_wrapper(A, m, B, n):

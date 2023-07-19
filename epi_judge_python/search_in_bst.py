67423567f39e75c9b6b8e8ebb7730a85e656e8dc
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2023-07-19 23:02:20 +0800

    question 14.0

--- epi_judge_python/search_in_bst.py
+++ epi_judge_python/search_in_bst.py
@@ -5,8 +5,13 @@ from test_framework import generic_test
 
 
 def search_bst(tree: BstNode, key: int) -> Optional[BstNode]:
-    # TODO - you fill in here.
-    return None
+    if not tree or tree.data==key:
+        return tree
+
+    if tree.data < key:
+        return search_bst(tree.right, key)
+    else:
+        return search_bst(tree.left, key)
 
 
 def search_bst_wrapper(tree, key):

f7a1d9b2d322078f820037bde74bb0fb7192c2b1
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2024-01-05 23:29:00 +0800

    question 14.3

--- epi_judge_python/k_largest_values_in_bst.py
+++ epi_judge_python/k_largest_values_in_bst.py
@@ -2,11 +2,39 @@ from typing import List
 
 from bst_node import BstNode
 from test_framework import generic_test, test_utils
+from collections import deque
 
+
+def find_k_largest_in_bst_inorder(tree, k):
+    """
+    Time: O(n)
+    Space: O(n)
+    """
+    bst_queue = deque()
+    def inorder(tree, k):
+        if not tree:
+            return 
+        inorder(tree.left, k)
+        bst_queue.append(tree.data)
+        inorder(tree.right, k)
+    inorder(tree, k)
+    return [bst_queue.pop() for _ in range(k)]
 
 def find_k_largest_in_bst(tree: BstNode, k: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    """
+    Time: O(h+k), where h is height of the tree
+    Space: O(h+k)
+    """
+    def reverse_inorder(tree, k):
+        if tree and len(result) < k:
+            reverse_inorder(tree.right, k)
+            if len(result) < k:
+                result.append(tree.data)
+                reverse_inorder(tree.left, k)
+
+    result = []
+    reverse_inorder(tree, k)
+    return result
 
 
 if __name__ == '__main__':

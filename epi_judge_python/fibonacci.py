79f75d5dd02fce22122da740d9362e6d81549b97
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2024-01-11 11:20:11 +0800

    question 16.0

--- epi_judge_python/fibonacci.py
+++ epi_judge_python/fibonacci.py
@@ -1,12 +1,32 @@
 from test_framework import generic_test
 
 
-def fibonacci(n: int) -> int:
-    # TODO - you fill in here.
-    return -1
+def fibonacci_bottomup_iter(n):
+    """
+    Time: O(n)
+    Space: O(1)
+    """
+    if n <= 1:
+        return n
+    f_minus_1, f_minus_2 = 1, 0
+    for _ in range(n-1):
+        f_n = f_minus_1 + f_minus_2
+        f_minus_1, f_minus_2 = f_n, f_minus_1
+    return f_n
+
+def fibonacci(n: int, cache={}) -> int:
+    """
+    Time: O(n)
+    Space: O(n)
+    """ 
+    if n <= 1:
+        return n
+    elif n not in cache:
+        cache[n] = fibonacci(n-1) + fibonacci(n-2)
+    return cache[n]
 
 
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main('fibonacci.py', 'fibonacci.tsv',
-                                       fibonacci))
+                                       fibonacci_bottomup_iter))
--- epi_judge_python/max_sum_subarray.py
+++ epi_judge_python/max_sum_subarray.py
@@ -1,11 +1,50 @@
 from typing import List
 
 from test_framework import generic_test
+import itertools
 
 
+def find_maximum_subarray_bf2(A):
+    """
+    Time: O(n^2)
+    Space: O(1)
+    """
+    n = len(A)
+    max_sum = 0
+
+    for i in range(n):
+        curr_sum = 0
+        for j in range(i, n):
+            curr_sum += A[j]
+            max_sum = max(max_sum, curr_sum)
+    return max_sum 
+
+def find_maximum_subarray_bf1(A):
+    """
+    Time: O(n^3)
+    Space: O(1)
+    """
+    n = len(A)
+    max_sum = 0
+    for subarray_size in range(1, n+1):
+        for start_idx in range(n):
+            end_idx = start_idx + subarray_size
+            if not end_idx > n:
+                max_sum = max(max_sum, sum(A[start_idx:end_idx]))
+
+    return max_sum
+    
+    
 def find_maximum_subarray(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return -1
+    """
+    Time: O(n)
+    Space: O(1)
+    """
+    max_sum = min_sum = 0
+    for running_sum in itertools.accumulate(A):
+        min_sum = min(running_sum, min_sum) 
+        max_sum = max(max_sum, running_sum-min_sum)
+    return max_sum
 
 
 if __name__ == '__main__':

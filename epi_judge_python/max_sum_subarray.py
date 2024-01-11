4d570059bf84d9b58c078814410ae0b63d97f76a
jasonli0707 <38065063+jasonli0707@users.noreply.github.com>
2024-01-11 12:53:26 +0800

    question 16.0 update

--- epi_judge_python/max_sum_subarray.py
+++ epi_judge_python/max_sum_subarray.py
@@ -46,9 +46,21 @@ def find_maximum_subarray(A: List[int]) -> int:
         max_sum = max(max_sum, running_sum-min_sum)
     return max_sum
 
+def find_maximum_subarray_sum(A):
+    """
+    Kadane's Algorithm
+    Time & Space: O(1)
+    """
+    max_sum = curr_sum = 0
+    for i in range(len(A)):
+        curr_sum = max(curr_sum+A[i], A[i]) # either continue to sum or restart from curr idx
+        max_sum = max(max_sum, curr_sum)
+
+    return max_sum
+
 
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main('max_sum_subarray.py',
                                        'max_sum_subarray.tsv',
-                                       find_maximum_subarray))
+                                       find_maximum_subarray_sum))
